60bf1af2499d39f5e6fa292bc3327c6773d6c94d
Dom Clemmer <thisisdom4@gmail.com>
2017-07-05 18:15:44 -0400

    express server w/ multer listening for & saving images

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict"
+
+var express = require('express')
+var multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+const app = express()
+
+app.post('/submit-image', upload.single('image'), function(req, res) {
+  console.log(req.file)
+})
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Server running on port ' + port)
+
+})
+
+app.on('error', function(){
+  console.log(error)
+})
+
+module.exports = app
