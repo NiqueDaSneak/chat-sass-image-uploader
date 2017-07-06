6fd7165e66a93cd2e6eb4163abb7f8b56d5881bc
Dom Clemmer <thisisdom4@gmail.com>
2017-07-05 20:08:38 -0400

    add og

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
-"use strict"
-
-var Express = require('express')
-var multer = require('multer')
-var bodyParser = require('body-parser')
-var app = Express()
-app.use(bodyParser.json())
+var Express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var app = Express();
+app.use(bodyParser.json());
 var Storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, "./Uploader")
+        callback(null, "./Images");
     },
     filename: function (req, file, callback) {
-        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname)
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
     }
-})
+});
 
-var upload = multer({ storage: Storage }).single('uploadedImage')
+var upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html")
-})
+    res.sendFile(__dirname + "/index.html");
+});
 
-app.post("/submit-image", function (req, res) {
+app.post("/api/Upload", function (req, res) {
     upload(req, res, function (err) {
         if (err) {
-            console.log("Something went wrong!")
+            console.log("Something went wrong!");
             console.log(err);
-        } else {
-          console.log("File uploaded sucessfully!.")
         }
-    })
-})
+        console.log("File uploaded sucessfully!.");
+    });
+});
 
 var port = process.env.PORT || 3000
 app.listen(port, function(){
