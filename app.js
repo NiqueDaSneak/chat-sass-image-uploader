4127f41eb62e6109c23b32fac1dbc92941c991d6
Dom Clemmer <thisisdom4@gmail.com>
2017-07-05 21:09:15 -0400

    add og

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = Express();
 app.use(bodyParser.json());
 var Storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, "./Images");
+        callback(null, "./Images/");
     },
     filename: function (req, file, callback) {
         callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
