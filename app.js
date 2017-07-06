77c716b9ae6581f4367088011ecc9b40af54a88f
Dom Clemmer <thisisdom4@gmail.com>
2017-07-06 10:12:33 -0400

    add mongoose

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ var express = require('express')
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' })
 
-var app = express();
+var app = express()
 
-app.post('/submit-image', upload.single('uploadedImage'), function (req, res, next) {
+// DATABASE SETUP
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://dom:Losangeleslakers47@ds123182.mlab.com:23182/chat-sass-frontend')
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+var messageSchema = mongoose.Schema({type: String, date: String, assetManifest: Object, organization: String, groupNames: Array, id: Number})
+var Message = mongoose.model('Message', messageSchema)
+
+app.post('/submit-data', upload.single('uploadedImage'), function (req, res, next) {
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
-  console.log(req.file);
-  console.log(req.body);
+  console.log(req.file)
+  console.log(req.body)
 })
-
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
 
 var port = process.env.PORT || 4000
 app.listen(port, function(){
